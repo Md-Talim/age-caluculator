d8dd0e4f6d09c1acd36a0410862d5667d95b9724
Mohammad Talim <talimmohammad116@gmail.com>
2023-07-27 09:45:45 +0530

    Make the card responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,34 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="bg-white py-12 px-6 w-[90.6%] flex gap-[30px] flex-col rounded-3xl rounded-ee-[200px]">
-      <section className="flex gap-[17px] justify-between items-center">
+    <main className="bg-white py-12 px-6 md:p-[55px] w-[90.6%] lg:w-fit flex gap-[30px] flex-col rounded-3xl rounded-ee-[200px]">
+      <section className="flex gap-[17px] md:w-fit">
         <InputField label="day" placeholder="DD" />
         <InputField label="month" placeholder="MM" />
         <InputField label="year" placeholder="YYYY" />
       </section>
-      <section className="relative h-[65px]">
-        <hr className="absolute h-[2px] -z-1 w-full top-1/2" />
-        <button className="absolute right-1/2 translate-x-1/2 mx-auto w-[65px] h-[65px] z-1 p-5 bg-primary flex items-center justify-center rounded-full">
-          <Image
-            src="/images/arrow.svg"
-            alt="arrow-icon"
-            width={65}
-            height={65}
-            className="w-[65px] h-[65px]"
-          />
-        </button>
+      <section className="relative h-[65px] md:flex md:w-fit">
+        <hr className="absolute lg:relative h-[2px] md:w-[633px] -z-1 w-full top-1/2" />
+        <div className="flex items-center justify-center lg:justify-end bg-transparent w-full md:w-auto">
+          <button className="absolute md:relative right-1/2 translate-x-1/2 translate-y-1/2 md:translate-y-0 mx-auto w-[65px] h-[65px] z-1 p-5 bg-primary flex items-center justify-center rounded-full md:w-[96px] md:h-[96px] md:p-[10px]">
+            <Image
+              src="/images/arrow.svg"
+              alt="arrow-icon"
+              width={65}
+              height={65}
+              className="w-[65px] h-[65px]"
+            />
+          </button>
+        </div>
       </section>
-      <section>
-        <p className="text-[56px] font-bold italic tracking-[-1.12px] leading-[62px] flex gap-2">
+      <section className="md:max-w-fit">
+        <p className="text-[56px] md:text-[102px] font-bold italic tracking-[-1.12px] leading-[62px] md:leading-[115px] text-offBlack flex gap-2 md:max-w-fit">
           <span className="text-primary">38</span>years
         </p>
-        <p className="text-[56px] font-bold italic tracking-[-1.12px] leading-[62px] flex gap-2">
+        <p className="text-[56px] md:text-[102px] font-bold italic tracking-[-1.12px] leading-[62px] md:leading-[115px] text-offBlack flex gap-2 md:max-w-fit">
           <span className="text-primary">3</span>months
         </p>
-        <p className="text-[56px] font-bold italic tracking-[-1.12px] leading-[62px] flex gap-2">
+        <p className="text-[56px] md:text-[102px] font-bold italic tracking-[-1.12px] leading-[62px] md:leading-[115px] text-offBlack flex gap-2 md:max-w-fit">
           <span className="text-primary">36</span>days
         </p>
       </section>
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,13 +5,13 @@ interface Props {
 
 const InputField = ({ label, placeholder }: Props) => {
   return (
-    <label className="flex flex-col max-w-[30%] gap-1 md:gap-2">
+    <label className="flex flex-col max-w-[30%] md:w-40 md:h-[100px] gap-1 md:gap-2">
       <span className="max-w-fit uppercase text-smokeGray text-sm font-bold tracking-[3.36px]">
         {label}
       </span>
       <input
         type="number"
-        className="text-smokeGray  text-[22px] md:text-[32px] font-bold leading-8 md:leading-9 tracking-[0.64px] md:px-6 px-[14px] md:py-4 py-[11px] border border-lightGray rounded-lg focus:outline-primary"
+        className="text-smokeGray md:w-40 text-[22px] md:text-[32px] font-bold leading-8 md:leading-9 tracking-[0.64px] md:px-6 px-[14px] md:py-4 py-[11px] border border-lightGray rounded-lg focus:outline-primary"
         placeholder={placeholder}
       />
     </label>
